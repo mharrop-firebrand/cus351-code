f960545f9f9da755d21a989a1946aff6a960b560
mharrop-firebrand <mharrop@oil.firebrand.training>
2023-10-24 16:03:38 +0100

    Updated part 5 from tuesday

---
+++ Python/PartFive/Activity/Actitivy.py
@@ -0,0 +1,41 @@
+#
+#! NOT WITH OPEN
+# Create a text file named "my_file.txt" and add the following content to it:
+# "Hello, this is the initial content of the file."
+# "This is the second line in the file."
+
+# Read the entire content of the file using the read() method and print it to the console.
+
+# Read the file line by line using the readlines() method and print each line (without newline characters) to the console.
+
+# Read the file using the readline() method and print the first and second lines to the console.
+
+# Append a new line to the file with the content: "This line is appended to the file."
+
+# Read the updated content of the file using the read() method and print it to the console.
+
+# Overwrite the entire file with the content: "This content overwrites the entire file."
+
+# Read the overwritten content of the file using the read() method and print it to the console.
+
+# Remove the file "my_file.txt," and make sure to handle the case where the file doesn't exist.
+
+# Verify that the file has been removed (or if it didn't exist, print a message indicating that).
+
+
+#! With Open
+
+# Open a new text file named "practice_file.txt" using the with open() statement.
+# Write some text into the file.
+
+# Read and print the content of "practice_file.txt" to the console using the with open() statement.
+
+# Append the line "This line is appended using 'with open()'." to the "practice_file.txt."
+
+# Read and print the updated content of "practice_file.txt" to the console using the with open() statement.
+
+# Remove "practice_file.txt," and handle the case where the file doesn't exist.
+
+
+#! Extra:
+# Look in to reading other file types, like csv or binary
